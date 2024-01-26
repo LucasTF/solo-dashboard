2640078de4b22729183967095c6fc3bc335424e6
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-01-26 14:05:58 -0300

    Created SearchBar component.

--- src/app/dashboard/clientes/page.tsx
+++ src/app/dashboard/clientes/page.tsx
@@ -1,43 +1,44 @@
 import { DataTable, TableRow } from "@/components/DataTable/DataTable";
 import Pagination from "@/components/Pagination/Pagination";
+import SearchBar from "@/components/SearchBar/SearchBar";
 
 export default function Dashboard() {
   const clients = [
     {
+      id: 1,
       name: "Lucas",
       email: "lucas.htferreira@outlook.com",
       phone: "11941410495",
     },
     {
+      id: 2,
       name: "Lucas",
       email: "lucas.htferreira@outlook.com",
       phone: "11941410495",
     },
     {
+      id: 3,
       name: "Lucas",
       email: "lucas.htferreira@outlook.com",
       phone: "11941410495",
     },
     {
+      id: 4,
       name: "Lucas",
       email: "lucas.htferreira@outlook.com",
       phone: "11941410495",
     },
   ];
 
-  const columnNames = ["Nome", "Email", "Telefone"];
+  const columnNames = ["Id", "Nome", "Email", "Telefone"];
 
   return (
     <main>
       <h1 className="font-bold text-4xl w-4/5 mx-auto my-4 text-white">
         Clientes
       </h1>
-      <section className="p-8 bg-slate-200 w-4/5 mx-auto rounded-md">
-        <input
-          type="text"
-          placeholder="Buscar"
-          className="w-full p-4 rounded-md"
-        />
+      <section className="p-8 bg-slate-200 w-4/5 mx-auto rounded-md shadow-lg">
+        <SearchBar />
 
         <p className="my-4 font-bold">
           {clients.length} resultado(s) encontrados.
@@ -58,7 +59,7 @@ export default function Dashboard() {
             type="button"
             className="bg-green-700 hover:bg-green-600 font-semibold text-white p-4 rounded-md"
           >
-            + Nova Inserção
+            + Novo Cliente
           </button>
         </div>
       </section>
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,5 @@
+export default function SearchBar() {
+  return (
+    <input type="text" placeholder="Buscar" className="w-full p-4 rounded-md" />
+  );
+}
