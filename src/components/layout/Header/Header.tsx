2eb720b3a3baa75f04aec1399a989a700adccb32
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-01-26 17:50:59 -0300

    Changed Header to Links.

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,12 +1,28 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const Header = () => {
+  const pathname = usePathname();
+
   return (
     <header className="py-4 flex justify-around bg-slate-200 sticky top-0 left-0">
       <h1 className="font-bold">Solo Dashboard</h1>
       <nav className="flex gap-4">
-        <a href="#">Clientes</a>
-        <a href="#">Obras Executadas</a>
-        <a href="#">Orçamentos</a>
-        <a href="#">Histórico</a>
+        <Link
+          href="/dashboard/clientes"
+          className={
+            pathname.startsWith("/dashboard/clientes")
+              ? "font-bold text-orange-500"
+              : ""
+          }
+        >
+          Clientes
+        </Link>
+        <Link href="#">Obras Executadas</Link>
+        <Link href="#">Orçamentos</Link>
+        <Link href="#">Histórico</Link>
       </nav>
     </header>
   );
