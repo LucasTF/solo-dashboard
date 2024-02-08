1f7e9ac642e71d62346e6d1d6e97abd2f9e1ffc0
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-02-08 17:26:55 -0300

    Definition of NewObraSchema.

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,8 +1,27 @@
+import { Logradouro } from "@/enums/Logradouro";
 import * as z from "zod";
 
 export const LoginSchema = z.object({
   email: z.string().email("Email inválido"),
   password: z.string().min(1, "Senha inválida"),
+});
+
+export const NewObraSchema = z.object({
+  nome: z.string().max(8),
+  num_obra: z.number().int().positive(),
+  ano: z.number().int().positive().min(1980),
+  data_inicio: z.date(),
+  data_fim: z.date(),
+  uf: z.string().min(2).max(2),
+  cidade: z.string().min(1).max(40),
+  tipo_logo: z.nativeEnum(Logradouro),
+  logradouro: z.string().min(1).max(40),
+  complemento: z.string().min(1).max(30),
+  bairro: z.string().min(1).max(40),
+  lote: z.string().max(40),
+  quadra: z.string().max(40),
+  proprietario: z.string().min(1).max(40),
+  cliente: z.string().min(1).max(40),
 });
 
 export const DashboardTableSearchSchema = z.object({
