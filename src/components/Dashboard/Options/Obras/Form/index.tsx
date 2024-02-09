85c37571b181736e601878743e77709173952bb8
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-02-09 12:00:50 -0300

    Implementation of validation on New Obra Form.

--- src/components/Dashboard/Options/Obras/Form/index.tsx
+++ src/components/Dashboard/Options/Obras/Form/index.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+import { NewObraSchema } from "@/schemas";
+import { Logradouro } from "@/enums/Logradouro";
+
 import Button from "@/components/ui/Button";
 import { Field } from "@/components/ui/Field";
-
-import { Logradouro } from "@/enums/Logradouro";
 
 type UF = {
   id: number;
@@ -28,73 +36,137 @@ type Municipio = {
   };
 };
 
-async function getUFs() {
-  const res = await fetch(
-    "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
-  );
-  return res.json();
-}
+type Obra = z.infer<typeof NewObraSchema>;
 
-async function getMunicipios(uf: string) {
-  const res = await fetch(
-    `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`
-  );
-  return res.json();
-}
-
-export const NewObraForm = async () => {
+export const NewObraForm = () => {
   const year = new Date().getFullYear();
   const years = Array.from(new Array(45), (_, index) => year - index);
 
-  const ufs: UF[] = await getUFs();
+  const [ufs, setUfs] = useState<UF[]>([]);
+  const [municipios, setMunicipios] = useState<Municipio[]>([]);
+
+  useEffect(() => {
+    const fetchSelects = async () => {
+      const ufFetch = fetch(
+        "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+      );
+      const muniFetch = fetch(
+        "https://servicodados.ibge.gov.br/api/v1/localidades/estados/SP/municipios"
+      );
+
+      const [ufData, muniData] = await Promise.all([ufFetch, muniFetch]);
+      const [ufJson, muniJson] = await Promise.all([
+        ufData.json(),
+        muniData.json(),
+      ]);
+
+      setUfs(ufJson);
+      setMunicipios(muniJson);
+    };
+
+    fetchSelects().catch((error) => console.log(error));
+  }, []);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Obra>({
+    resolver: zodResolver(NewObraSchema),
+  });
 
   return (
-    <form className="m-4">
+    <form
+      className="m-4"
+      onSubmit={handleSubmit((formData) => console.log(formData))}
+    >
       <section className="grid lg:grid-cols-2 gap-8">
         <div className="flex flex-col gap-y-4">
-          <Field.Input label="Código SP" />
-          <Field.Input label="Número SP" />
+          <Field.Input
+            label="Código SP"
+            isInvalid={!!errors.nome}
+            errorMessage={errors.nome?.message}
+            {...register("nome")}
+          />
+          <Field.Input
+            label="Número SP"
+            isInvalid={!!errors.num_obra}
+            errorMessage={errors.num_obra?.message}
+            {...register("num_obra")}
+          />
         </div>
 
         <aside className="grid grid-cols-2 gap-4 border-2 border-slate-300 border-solid rounded-md p-4">
-          <Field.Input label="Sondagens" />
-          <Field.Input label="Total Metros" />
-          <Field.Input label="Trado" defaultValue={0} />
-          <Field.Input label="Total Metros Trado" defaultValue={0} />
+          <Field.Input
+            label="Sondagens"
+            isInvalid={!!errors.sp_sondagem}
+            errorMessage={errors.sp_sondagem?.message}
+            {...register("sp_sondagem")}
+          />
+          <Field.Input
+            label="Total Metros"
+            isInvalid={!!errors.metros_sp_sondagem}
+            errorMessage={errors.metros_sp_sondagem?.message}
+            {...register("metros_sp_sondagem")}
+          />
+          <Field.Input
+            label="Trado"
+            defaultValue={0}
+            isInvalid={!!errors.STTrado}
+            errorMessage={errors.STTrado?.message}
+            {...register("STTrado")}
+          />
+          <Field.Input
+            label="Total Metros Trado"
+            defaultValue={0}
+            isInvalid={!!errors.STTradoml}
+            errorMessage={errors.STTradoml?.message}
+            {...register("STTradoml")}
+          />
         </aside>
       </section>
 
       <hr className="m-4" />
 
       <section className="grid lg:grid-cols-3 gap-4">
-        <Field.Select label="Ano da Obra">
+        <Field.Select
+          label="Ano da Obra"
+          isInvalid={!!errors.ano}
+          errorMessage={errors.ano?.message}
+          {...register("ano")}
+        >
           {years.map((year, index) => (
             <option key={index} value={year}>
               {year}
             </option>
           ))}
         </Field.Select>
-        <Field.Input label="Data de Início" type="date" />
-        <Field.Input label="Data de Finalização" type="date" />
+        <Field.Input
+          label="Data de Início"
+          type="date"
+          isInvalid={!!errors.data_inicio}
+          errorMessage={errors.data_inicio?.message}
+          {...register("data_inicio")}
+        />
+        <Field.Input
+          label="Data de Finalização"
+          type="date"
+          isInvalid={!!errors.data_fim}
+          errorMessage={errors.data_fim?.message}
+          {...register("data_fim")}
+        />
       </section>
 
       <hr className="m-4" />
 
       <section className="grid lg:grid-cols-3 gap-4">
         <aside className="grid gap-4 col-span-1">
-          <Field.Select label="UF">
-            <option value="SP">São Paulo</option>
-            {ufs.map((uf) => {
-              if (uf.sigla === "SP") return;
-              return (
-                <option key={uf.sigla} value={uf.sigla}>
-                  {uf.nome}
-                </option>
-              );
-            })}
-          </Field.Select>
-          <Field.Select label="Cidade"></Field.Select>
-          <Field.Select label="Tipo Logradouro">
+          <Field.Select
+            label="Tipo Logradouro"
+            isInvalid={!!errors.tipo_logo}
+            errorMessage={errors.tipo_logo?.message}
+            {...register("tipo_logo")}
+          >
             {Object.values(Logradouro)
               .filter((v) => isNaN(Number(v)))
               .map((log, index) => {
@@ -109,16 +181,71 @@ export const NewObraForm = async () => {
                 );
               })}
           </Field.Select>
+          <Field.Select
+            label="UF"
+            isInvalid={!!errors.uf}
+            errorMessage={errors.uf?.message}
+            {...register("uf")}
+          >
+            <option value="SP">São Paulo</option>
+            {ufs.map((uf) => {
+              if (uf.sigla === "SP") return;
+              return (
+                <option key={uf.sigla} value={uf.sigla}>
+                  {uf.nome}
+                </option>
+              );
+            })}
+          </Field.Select>
+          <Field.Select
+            label="Cidade"
+            isInvalid={!!errors.cidade}
+            errorMessage={errors.cidade?.message}
+            {...register("cidade")}
+          >
+            {municipios.map((municipio) => {
+              return (
+                <option key={municipio.id} value={municipio.nome}>
+                  {municipio.nome}
+                </option>
+              );
+            })}
+          </Field.Select>
         </aside>
         <section className="grid gap-4 lg:col-span-2">
           <div className="grid lg:grid-flow-col gap-4">
-            <Field.Input label="Logradouro" />
-            <Field.Input label="Complemento" />
+            <Field.Input
+              label="Logradouro"
+              isInvalid={!!errors.logradouro}
+              errorMessage={errors.logradouro?.message}
+              {...register("logradouro")}
+            />
+            <Field.Input
+              label="Complemento"
+              isInvalid={!!errors.complemento}
+              errorMessage={errors.complemento?.message}
+              {...register("complemento")}
+            />
           </div>
-          <Field.Input label="Bairro" />
+          <Field.Input
+            label="Bairro"
+            isInvalid={!!errors.bairro}
+            errorMessage={errors.bairro?.message}
+            {...register("bairro")}
+          />
           <div className="grid lg:grid-flow-col gap-4">
-            <Field.Input label="Lote" />
-            <Field.Input label="Quadra" />
+            <Field.Input
+              label="Lote"
+              isInvalid={!!errors.lote}
+              errorMessage={errors.lote?.message}
+              {...register("lote")}
+            />
+            <Field.Input
+              label="Quadra"
+              isInvalid={!!errors.quadra}
+              errorMessage={errors.quadra?.message}
+              {...register("quadra")}
+            />
           </div>
         </section>
       </section>
@@ -126,8 +253,18 @@ export const NewObraForm = async () => {
       <hr className="m-4" />
 
       <section className="grid gap-4">
-        <Field.Input label="Proprietário" />
-        <Field.Input label="Cliente" />
+        <Field.Input
+          label="Proprietário"
+          isInvalid={!!errors.proprietario}
+          errorMessage={errors.proprietario?.message}
+          {...register("proprietario")}
+        />
+        <Field.Input
+          label="Cliente"
+          isInvalid={!!errors.cliente}
+          errorMessage={errors.cliente?.message}
+          {...register("cliente")}
+        />
       </section>
 
       <section className="flex flex-row-reverse mt-4">
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,27 +1,47 @@
 import { Logradouro } from "@/enums/Logradouro";
 import * as z from "zod";
 
+const NUMBER = "Valor deve ser um número.";
+const POSITIVE = "Valor deve ser maior ou igual a 0.";
+const GREATER_THAN_0 = "Valor deve ser maior que 0.";
+const CANNOT_BE_EMPTY = "Campo obrigatório.";
+
 export const LoginSchema = z.object({
   email: z.string().email("Email inválido"),
   password: z.string().min(1, "Senha inválida"),
 });
 
 export const NewObraSchema = z.object({
-  nome: z.string().max(8),
-  num_obra: z.number().int().positive(),
-  ano: z.number().int().positive().min(1980),
-  data_inicio: z.date(),
-  data_fim: z.date(),
+  nome: z.string().max(8, "Deve conter apenas 8 caracteres"),
+  num_obra: z.coerce.number().int().min(1, GREATER_THAN_0).max(999),
+  sp_sondagem: z.coerce
+    .number({ invalid_type_error: NUMBER })
+    .int()
+    .nonnegative(POSITIVE),
+  metros_sp_sondagem: z.coerce
+    .number({ invalid_type_error: NUMBER })
+    .nonnegative(POSITIVE),
+  STTrado: z.coerce
+    .number({ invalid_type_error: NUMBER })
+    .int()
+    .nonnegative(POSITIVE),
+  STTradoml: z.coerce
+    .number({ invalid_type_error: NUMBER })
+    .int()
+    .nonnegative(POSITIVE),
+  ano: z.coerce.number().int().positive().min(1980),
+  data_inicio: z.coerce.date(),
+  data_fim: z.coerce.date(),
   uf: z.string().min(2).max(2),
   cidade: z.string().min(1).max(40),
   tipo_logo: z.nativeEnum(Logradouro),
-  logradouro: z.string().min(1).max(40),
-  complemento: z.string().min(1).max(30),
-  bairro: z.string().min(1).max(40),
+  logradouro: z.string().min(1, CANNOT_BE_EMPTY).max(40),
+  complemento: z.string().min(1, CANNOT_BE_EMPTY).max(30),
+  bairro: z.string().min(1, CANNOT_BE_EMPTY).max(40),
   lote: z.string().max(40),
   quadra: z.string().max(40),
-  proprietario: z.string().min(1).max(40),
-  cliente: z.string().min(1).max(40),
+  proprietario: z.string().min(1, CANNOT_BE_EMPTY).max(40),
+  cliente: z.string().min(1, CANNOT_BE_EMPTY).max(40),
 });
 
 export const DashboardTableSearchSchema = z.object({
