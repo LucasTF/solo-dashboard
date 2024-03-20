616ed872077e186c6070ba51007e95ed7d5ae264
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-03-20 10:55:04 -0300

    Improvements to tmux script.

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
 
+# Check if tmux package is installed
 if ! command -v tmux &> /dev/null
 then
     echo "tmux not installed."
     exit 1
 fi
 
-session="soloapp"
+SESSION="soloapp"
+SKIP_BUILD_STEP=false
+COMMAND=""
 
-tmux new-session -d -s "$session"
-tmux send-keys -t "$session" 'npm run build && npm run start' Enter
+# Check if "--no-build" flag is present
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -nb | --no-build)
+            SKIP_BUILD_STEP=true
+        ;;
+        *)
+            echo "ERROR! Unknown flag"
+            exit 2
+        ;;
+    esac
+    shift
+done
 
-echo "tmux session created as $session"
-echo "Enter session by typing 'tmux attach -t $session'"
+if [ "$SKIP_BUILD_STEP" = true ];
+then
+    COMMAND='npm run start'
+else
+    COMMAND='npm run build && npm run start'
+fi
+
+# Create new tmux session
+tmux new-session -d -s "$SESSION"
+
+# Send command to tmux session
+tmux send-keys -t "$SESSION" "$COMMAND" Enter
+
+echo "tmux session created as $SESSION"
+echo "Enter session by typing 'tmux attach -t $SESSION'"
+
+exit 0
