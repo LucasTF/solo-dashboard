783102d0882d5b42848cd0f0761420801501e120
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-05-15 10:33:49 -0300

    Beginning of port to Flask/Python Backend.

---
+++ server/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+from routes.static import static_content
+
+app = Flask(__name__, template_folder='out', static_folder='out/_next')
+app.register_blueprint(static_content)
+
+# App Routes
+@app.route('/', methods=['GET'])
+def home_page():
+    return '<h1>Hello World</h1>'
+
+@app.route('/login', methods=['GET'])
+def login_page():
+    return render_template('login.html')
+
+@app.route('/dashboard', methods=['GET'])
+def dashboard_page():
+    return render_template('dashboard.html')
+
+# Error Handler
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ server/routes/static.py
@@ -0,0 +1,17 @@
+import os
+from flask import Blueprint, abort, send_file
+
+static_content = Blueprint('static_content', __name__, template_folder='out')
+
+# Favicon
+@static_content.route('/favicon.ico')
+def favicon():
+    return send_file('out/favicon.ico')
+
+# Images
+@static_content.route('/img/<path:subpath>')
+def get_image(subpath):
+    if os.path.isfile(f'out/img/{subpath}'):
+        return send_file(f'out/img/{subpath}')
+    
+    abort(404)
