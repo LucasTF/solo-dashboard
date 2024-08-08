9c2cbfa400a9ec963c1a47b2f3237f70a8313d95
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-08-08 17:06:41 -0300

    server: Implementation of Usuario Entity.

---
+++ server/src/models/entities/usuario.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Boolean, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.models.entities.base import Base
+
+class Usuario(Base):
+    __tablename__ = 'Usuario'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(100), index=True)
+    email: Mapped[str] = mapped_column(String(100), index=True)
+    password: Mapped[str] = mapped_column(String(100))
+    is_admin: Mapped[bool] = mapped_column(Boolean())
+
+    def __repr__(self) -> str:
+        return f"<Usuario | id={self.id} | name={self.name} | email={self.email} | is_admin={self.is_admin}"
