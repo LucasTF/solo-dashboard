d6d3dc6be804af75fcc8787b32424706e087b967
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-08-09 11:42:59 -0300

    server: Add session handler to DBConnector.

--- server/src/database/connector.py
+++ server/src/database/connector.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
@@ -10,11 +11,19 @@ class DBConnector:
 
         self.__conection_string = os.getenv("DATABASE_CONN_STRING")
         self.__engine = None
+        self.session = None
 
     def connect_to_db(self):
         self.__engine = create_engine(self.__conection_string)
 
     def get_engine(self):
         return self.__engine
+    
+    def __enter__(self):
+        session_maker = sessionmaker()
+        self.session = session_maker(bind=self.__engine)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
     
 db_connector = DBConnector()
