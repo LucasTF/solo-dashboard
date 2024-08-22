ab56da169f1fef2f87a743125a71903bb8707870
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-08-22 17:33:39 -0300

    test: Implementation of find_by_id and find_by_email UsuarioController tests.

--- server/src/controllers/test_usuario_controller.py
+++ server/src/controllers/test_usuario_controller.py
@@ -3,12 +3,14 @@ from unittest.mock import Mock
 
 from src.controllers.usuario_controller import UsuarioController
 from src.models.serials.serial_response import SerialResponse
+from src.models.serials.serial_usuario import SerialUsuario
 
 
 class UsuarioControllerTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.__controller = UsuarioController(Mock())
+        self.__repository = Mock()
+        self.__controller = UsuarioController(self.__repository)
 
     def test_create_without_id(self):
 
@@ -68,3 +70,59 @@ class UsuarioControllerTestCase(unittest.TestCase):
         self.assertIsInstance(result, SerialResponse)
         self.assertNotEqual(result.message, 'Usuário criado com sucesso.')
         self.assertEqual(result.details, 'Dados inválidos.')
+
+    def test_find_by_id(self):
+
+        sample_usuario = SerialUsuario(
+            id=1,
+            name="John Doe",
+            email="email@test.com",
+            password="123456",
+            is_admin=False
+        )
+
+        self.__repository.get_usuario_by_id.return_value = sample_usuario
+
+        result = self.__controller.find_by_id(1)
+
+        self.__repository.get_usuario_by_id.assert_called_once_with(1)
+        self.assertEqual(result, sample_usuario)
+
+    def test_find_by_id_not_found(self):
+
+        self.__repository.get_usuario_by_id.return_value = None
+
+        result = self.__controller.find_by_id(1)
+
+        self.__repository.get_usuario_by_id.assert_called_once_with(1)
+        self.assertIsNone(result)
+
+    def test_find_by_email(self):
+
+        sample_email = "email@test.com"
+
+        sample_usuario = SerialUsuario(
+            id=1,
+            name="John Doe",
+            email=sample_email,
+            password="123456",
+            is_admin=False
+        )
+
+        self.__repository.get_usuario_by_email.return_value = sample_usuario
+
+        result = self.__controller.find_by_email(sample_email)
+
+        self.__repository.get_usuario_by_email.assert_called_once_with(sample_email)
+        self.assertEqual(result, sample_usuario)
+
+    def test_find_by_email_not_found(self):
+
+        sample_email = "email@test.com"
+
+        self.__repository.get_usuario_by_email.return_value = None
+
+        result = self.__controller.find_by_email(sample_email)
+
+        self.__repository.get_usuario_by_email.assert_called_once_with(sample_email)
+        self.assertIsNone(result)
