b78b1b07cb7bf31d8d0240d473521df97a45b452
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-09-25 17:42:52 -0300

    server-feature: Implementation of ClienteController validation.

--- server/src/controllers/cliente_controller.py
+++ server/src/controllers/cliente_controller.py
@@ -3,6 +3,7 @@ from src.controllers.interfaces.cliente_controller_interface import ClienteContr
 from src.errors.unavailable_resource_error import UnavailableResourceError
 from src.models.entities.cliente import Cliente
 from src.models.interfaces.cliente_repository_interface import ClienteRepositoryInterface
+from src.models.serials.serial_cliente import SerialCliente
 
 
 class ClienteController(ClienteControllerInterface):
@@ -11,7 +12,7 @@ class ClienteController(ClienteControllerInterface):
         self.__repository = clienteRepository
 
     def create(self, cliente_info: Dict) -> None:
-        raise NotImplementedError
+        self.__validate_cliente(cliente_info)
 
     def find_by_id(self, cliente_id: int) -> Cliente:
         cliente = self.__repository.get_cliente_by_id(cliente_id)
@@ -30,3 +31,23 @@ class ClienteController(ClienteControllerInterface):
         clientes = self.__repository.search_clientes(search_string)
 
         return clientes
+    
+    def __validate_cliente(self, cliente_dict: Dict) -> SerialCliente:
+        serialized_cliente = SerialCliente(
+            nome=cliente_dict.get('nome'), 
+            apelido=cliente_dict.get('apelido'),
+            cpf=cliente_dict.get('cpf'),
+            cnpj=cliente_dict.get('cnpj'),
+            tipo_logo=cliente_dict.get('tipo_logo'),
+            logradouro=cliente_dict.get('logradouro'),
+            complemento=cliente_dict.get('complemento'),
+            bairro=cliente_dict.get('bairro'),
+            cidade=cliente_dict.get('cidade'),
+            uf=cliente_dict.get('uf'),
+            cep=cliente_dict.get('cep'),
+            email=cliente_dict.get('email'),
+            fone1=cliente_dict.get('fone1'),
+            fone2=cliente_dict.get('fone2'),
+        )
+
+        return serialized_cliente
