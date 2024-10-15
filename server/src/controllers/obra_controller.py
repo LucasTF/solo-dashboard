ef28e2752e2c3f628762f2cb834f37d054ed4e25
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-10-15 15:10:19 -0300

    server-change: Refactoring of ObraController __validate_edit_info and __validate_obra private methods.

--- server/src/controllers/obra_controller.py
+++ server/src/controllers/obra_controller.py
@@ -3,7 +3,6 @@ from src.controllers.interfaces.obra_controller_interface import ObraControllerI
 from src.errors.unavailable_resource_error import UnavailableResourceError
 from src.models.interfaces.obra_repository_interface import ObraRepositoryInterface
 from src.types.obra_response import ObraResponse
-from src.validators.valid_cliente import ValidCliente
 from src.validators.valid_obra import ValidObra
 
 
@@ -58,7 +57,9 @@ class ObraController(ObraControllerInterface):
         return obras
 
     def update(self, obra_id: int, obra_info: Dict) -> None:
-        self.__validate_edit_info(obra_info)
+        obra = self.find_by_id(obra_id)
+
+        self.__validate_edit_info(obra, obra_info)
         self.__repository.update_obra(
             id=obra_id,
             cod_obra=obra_info.get("cod_obra"),
@@ -84,75 +85,58 @@ class ObraController(ObraControllerInterface):
 
         return obras
 
-    def __validate_edit_info(self, obra_info: Dict) -> None:
-        if obra_info.get("cod_obra"):
-            ValidObra.__pydantic_validator__.validate_assignment(
-                ValidObra.model_construct(), "cod_obra", obra_info["cod_obra"]
-            )
-        if obra_info.get("num_obra"):
-            ValidObra.__pydantic_validator__.validate_assignment(
-                ValidObra.model_construct(), "num_obra", obra_info["num_obra"]
-            )
-        if obra_info.get("ano"):
-            ValidObra.__pydantic_validator__.validate_assignment(
-                ValidObra.model_construct(), "ano", obra_info["ano"]
-            )
-        if obra_info.get("data_inicio"):
-            ValidObra.__pydantic_validator__.validate_assignment(
-                ValidObra.model_construct(), "data_inicio", obra_info["data_inicio"]
-            )
-        if obra_info.get("data_fim"):
-            ValidObra.__pydantic_validator__.validate_assignment(
-                ValidObra.model_construct(), "data_fim", obra_info["data_fim"]
-            )
-        if obra_info.get("tipo_logo"):
-            ValidObra.__pydantic_validator__.validate_assignment(
-                ValidObra.model_construct(), "tipo_logo", obra_info["tipo_logo"]
-            )
-        if obra_info.get("logradouro"):
-            ValidObra.__pydantic_validator__.validate_assignment(
-                ValidObra.model_construct(), "logradouro", obra_info["logradouro"]
-            )
-        if obra_info.get("lote"):
-            ValidObra.__pydantic_validator__.validate_assignment(
-                ValidObra.model_construct(), "lote", obra_info["lote"]
-            )
-        if obra_info.get("quadra"):
-            ValidObra.__pydantic_validator__.validate_assignment(
-                ValidObra.model_construct(), "quadra", obra_info["quadra"]
-            )
-        if obra_info.get("bairro"):
-            ValidObra.__pydantic_validator__.validate_assignment(
-                ValidObra.model_construct(), "bairro", obra_info["bairro"]
-            )
-        if obra_info.get("cidade"):
-            ValidObra.__pydantic_validator__.validate_assignment(
-                ValidObra.model_construct(), "cidade", obra_info["cidade"]
-            )
-        if obra_info.get("uf"):
-            ValidObra.__pydantic_validator__.validate_assignment(
-                ValidObra.model_construct(), "uf", obra_info["uf"]
-            )
-        if obra_info.get("cep"):
-            ValidObra.__pydantic_validator__.validate_assignment(
-                ValidObra.model_construct(), "cep", obra_info["cep"]
-            )
-        if obra_info.get("complemento"):
-            ValidObra.__pydantic_validator__.validate_assignment(
-                ValidObra.model_construct(), "complemento", obra_info["complemento"]
-            )
-        if obra_info.get("cliente"):
-            self.__validate_cliente(obra_info["cliente"])
-        if obra_info.get("proprietario"):
-            self.__validate_cliente(obra_info["proprietario"])
-
-    def __validate_cliente(self, cliente_name: str) -> None:
-        try:
-            ValidCliente.__pydantic_validator__.validate_assignment(
-                ValidCliente.model_construct(), "nome", cliente_name
-            )
-        except ValueError:
-            raise ValueError("Nome do cliente/proprietário é inválido.")
+    def __validate_edit_info(self, obra: ObraResponse, update_info: Dict) -> None:
+        edit_obra_dict = {
+            "id": obra.id,
+            "cod_obra": update_info["cod_obra"]
+            if update_info.get("cod_obra") is not None
+            else obra.cod_obra,
+            "num_obra": update_info["num_obra"]
+            if update_info.get("num_obra") is not None
+            else obra.num_obra,
+            "ano": update_info["ano"]
+            if update_info.get("ano") is not None
+            else obra.ano,
+            "data_inicio": update_info["data_inicio"]
+            if update_info.get("data_inicio") is not None
+            else obra.data_inicio,
+            "data_fim": update_info["data_fim"]
+            if update_info.get("data_fim") is not None
+            else obra.data_fim,
+            "cidade": update_info["cidade"]
+            if update_info.get("cidade") is not None
+            else obra.cidade,
+            "uf": update_info["uf"] if update_info.get("uf") is not None else obra.uf,
+            "logradouro": update_info["logradouro"]
+            if update_info.get("logradouro") is not None
+            else obra.logradouro,
+            "bairro": update_info["bairro"]
+            if update_info.get("bairro") is not None
+            else obra.bairro,
+            "cliente": update_info["cliente"]
+            if update_info.get("cliente") is not None
+            else obra.cliente,
+            "tipo_logo": update_info["tipo_logo"]
+            if update_info.get("tipo_logo") is not None
+            else obra.tipo_logo,
+            "complemento": update_info["complemento"]
+            if update_info.get("complemento") is not None
+            else obra.complemento,
+            "lote": update_info["lote"]
+            if update_info.get("lote") is not None
+            else obra.lote,
+            "quadra": update_info["quadra"]
+            if update_info.get("quadra") is not None
+            else obra.quadra,
+            "cep": update_info["cep"]
+            if update_info.get("cep") is not None
+            else obra.cep,
+            "proprietario": update_info["proprietario"]
+            if update_info.get("proprietario") is not None
+            else obra.proprietario,
+        }
+        obra_to_validate = ObraResponse(**edit_obra_dict)
+        ValidObra.serialize(obra_to_validate)
 
     def __validate_obra(self, obra_info: Dict) -> ValidObra:
         valid_obra = ValidObra(
@@ -170,6 +154,8 @@ class ObraController(ObraControllerInterface):
             uf=obra_info.get("uf"),
             cep=obra_info.get("cep"),
             complemento=obra_info.get("complemento"),
+            cliente=obra_info.get("cliente"),
+            proprietario=obra_info.get("proprietario"),
         )
 
         return valid_obra
