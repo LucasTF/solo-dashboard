77c275eda25a392a2b5eb8e4b94cb039623da23a
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-11-11 17:25:12 -0300

    server-fix: Update to static routes.

--- server/src/core/routes/static.py
+++ server/src/core/routes/static.py
@@ -1,19 +1,24 @@
 import os
 from flask import Blueprint, abort, send_file
 
-static_content = Blueprint("static_content", __name__, template_folder="out")
+dir = os.getcwd() + "/src/views/out"
+static_content = Blueprint("static_content", __name__)
 
 
 # Favicon
 @static_content.route("/favicon.ico")
 def favicon():
-    return send_file("out/favicon.ico")
+    return send_file(dir + "/favicon.ico")
 
+# Static (CSS/JS)
+@static_content.route("/_next/<path:subpath>")
+def get_static(subpath):
+    return send_file(f"{dir}/_next/{subpath}")
 
 # Images
 @static_content.route("/img/<path:subpath>")
 def get_image(subpath):
-    if os.path.isfile(f"out/img/{subpath}"):
-        return send_file(f"out/img/{subpath}")
+    if os.path.isfile(f"{dir}/img/{subpath}"):
+        return send_file(f"{dir}/img/{subpath}")
 
     abort(404)
