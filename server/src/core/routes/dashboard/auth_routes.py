e105af5618f8cef674d3f28ef3220a40b5415e3d
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-11-25 17:21:00 -0300

    server-change: Separate login route and add special handler for UnauthenticatedError exception.

---
+++ server/src/core/routes/dashboard/auth_routes.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, redirect, render_template, send_file
+
+from src.config.dashboard import dashboard_template
+from src.core.middlewares.auth_middleware import check_authentication
+from src.errors.unauthenticated_error import UnauthenticatedError
+
+
+dashboard_auth_route = Blueprint("dashboard_auth_routes", __name__)
+
+# Login
+@dashboard_auth_route.route("/login", methods=["GET"])
+def login():
+    try:
+        check_authentication()
+        return redirect("/dashboard")
+    except UnauthenticatedError:
+        return render_template("/dashboard/login.html")
+
+
+@dashboard_auth_route.route("/login.txt", methods=["GET"])
+def login_txt():
+    return send_file(f"{dashboard_template}/dashboard/login.txt")
--- server/src/core/routes/dashboard/dashboard_routes.py
+++ server/src/core/routes/dashboard/dashboard_routes.py
@@ -1,71 +1,55 @@
 import os
 from flask import Blueprint, redirect, render_template, send_file
 
+from src.config.dashboard import dashboard_template
 from src.core.middlewares.auth_middleware import check_authentication
 from src.errors.unauthenticated_error import UnauthenticatedError
 
-template_dir = os.getcwd() + "/src/views/out"
+# Routes
+
+from src.core.routes.dashboard.auth_routes import dashboard_auth_route
+
 dashboard_route = Blueprint(
     "dashboard_routes",
     __name__,
-    template_folder=template_dir,
-    static_folder=template_dir,
+    template_folder=dashboard_template,
+    static_folder=dashboard_template,
     url_prefix="/dashboard",
 )
 
+# Errors
+
+dashboard_route.register_error_handler(UnauthenticatedError, lambda _: redirect("/dashboard/login"))
+
+dashboard_route.register_blueprint(dashboard_auth_route)
 
 @dashboard_route.route("/", methods=["GET"])
 def index():
-    try:
-        check_authentication()
-    except UnauthenticatedError:
-        return redirect("/dashboard/login")
+    check_authentication()
 
     return render_template("dashboard.html")
-
-
-# Login
-@dashboard_route.route("/login", methods=["GET"])
-def login():
-    try:
-        check_authentication()
-        return redirect("/dashboard")
-    except UnauthenticatedError:
-        return render_template("/dashboard/login.html")
-
-
-@dashboard_route.route("/login.txt", methods=["GET"])
-def login_txt():
-    return send_file(f"{template_dir}/dashboard/login.txt")
-
 
 # Obras
 @dashboard_route.route("/obras", methods=["GET"])
 def obras():
-    try:
-        check_authentication()
-    except UnauthenticatedError:
-        return redirect("/dashboard/login")
+    check_authentication()
 
     return render_template("/dashboard/obras.html")
 
 
 @dashboard_route.route("/obras.txt", methods=["GET"])
 def obras_txt():
-    return send_file(f"{template_dir}/dashboard/obras.txt")
+    return send_file(f"{dashboard_template}/dashboard/obras.txt")
 
 
 # New Obra
 @dashboard_route.route("/obras/new", methods=["GET"])
 def new_obra():
-    try:
-        check_authentication()
-    except UnauthenticatedError:
-        return redirect("/dashboard/login")
+    check_authentication()
 
     return render_template("/dashboard/obras/new.html")
 
 
 @dashboard_route.route("/obras/new.txt", methods=["GET"])
 def new_obra_txt():
-    return send_file(f"{template_dir}/dashboard/obras/new.txt")
+    return send_file(f"{dashboard_template}/dashboard/obras/new.txt")
