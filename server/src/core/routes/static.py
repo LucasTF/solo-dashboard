a1383a650d2114bd2d932b667a8493939e5b1c67
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-11-26 16:30:16 -0300

    server-fix: Change static routes to use dashboard template config.

--- server/src/core/routes/static.py
+++ server/src/core/routes/static.py
@@ -1,27 +1,28 @@
 import os
 from flask import Blueprint, abort, send_file
 
-dir = os.getcwd() + "/src/views/out"
+from src.config.dashboard import dashboard_template
+
 static_content = Blueprint("static_content", __name__)
 
 
 # Favicon
 @static_content.route("/favicon.ico")
 def favicon():
-    return send_file(dir + "/favicon.ico")
+    return send_file(dashboard_template + "/favicon.ico")
 
 
 # Static (CSS/JS)
 @static_content.route("/_next/<path:subpath>")
 def get_static(subpath):
-    return send_file(f"{dir}/_next/{subpath}")
+    return send_file(f"{dashboard_template}/_next/{subpath}")
 
 
 # Images
 @static_content.route("/img/<path:subpath>")
 def get_image(subpath):
-    if os.path.isfile(f"{dir}/img/{subpath}"):
-        return send_file(f"{dir}/img/{subpath}")
+    if os.path.isfile(f"{dashboard_template}/img/{subpath}"):
+        return send_file(f"{dashboard_template}/img/{subpath}")
 
     abort(404)
 
@@ -29,4 +30,4 @@ def get_image(subpath):
 # Routes TXT
 @static_content.route("/dashboard.txt")
 def dashboard_txt():
-    return send_file(dir + "/dashboard.txt")
+    return send_file(dashboard_template + "/dashboard.txt")
