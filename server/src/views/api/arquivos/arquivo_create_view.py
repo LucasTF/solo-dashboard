26164998c684549511b90bc7edee93323aa10f7c
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-11-29 17:45:31 -0300

    server-feature: Implementation of ArquivoCreateView.

--- server/src/views/api/arquivos/arquivo_create_view.py
+++ server/src/views/api/arquivos/arquivo_create_view.py
@@ -1,7 +1,9 @@
 from typing import List
 from werkzeug.datastructures import FileStorage
 
-from src.controllers.interfaces.arquivo_controller_interface import ArquivoControllerInterface
+from src.controllers.interfaces.arquivo_controller_interface import (
+    ArquivoControllerInterface,
+)
 from src.views.api.interfaces.view_interface import ViewInterface
 from src.views.api.types.http_request import HttpRequest
 from src.views.api.types.http_response import HttpResponse
@@ -13,13 +15,13 @@ class ArquivoCreateView(ViewInterface):
 
     def handle(self, http_request: HttpRequest) -> HttpResponse:
         obra_id = http_request.params.get("id")
-        files : List[FileStorage] = http_request.body.get("files")
+        files: List[FileStorage] = http_request.body.get("files")
 
         inserted_ids = self.__controller.create(obra_id, files)
         body_response = []
 
         for id in inserted_ids:
             arquivo = self.__controller.find_by_id(id)
-            body_response.append(arquivo)
+            body_response.append(arquivo.to_dict())
 
         return HttpResponse(status_code=201, body=body_response)
