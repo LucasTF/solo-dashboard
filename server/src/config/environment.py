773624d2960500373ddaeb8cb720665f48c39efe
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-11-29 17:35:28 -0300

    server-change: Add default values for some environment variables.

--- server/src/config/environment.py
+++ server/src/config/environment.py
@@ -5,8 +5,10 @@ db_env = {"DATABASE_CONN_STRING": os.getenv("DATABASE_CONN_STRING")}
 
 jwt_env = {
     "JWT_SECRET": os.getenv("JWT_SECRET"),
-    "JWT_ALGORITHM": os.getenv("JWT_ALGORITHM"),
-    "JWT_HOURS": os.getenv("JWT_HOURS"),
+    "JWT_ALGORITHM": os.getenv("JWT_ALGORITHM") or "HS256",
+    "JWT_HOURS": os.getenv("JWT_HOURS") or 720,
 }
 
-files_env = {"UPLOADED_FILES_PATH": os.getenv("UPLOADED_FILES_PATH")}
+files_env = {
+    "UPLOADED_FILES_PATH": os.getenv("UPLOADED_FILES_PATH"),
+}
